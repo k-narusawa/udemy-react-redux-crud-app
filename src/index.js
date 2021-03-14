c1830d2d14d7e4a0deda5e8ec6c0c47186be36c4
k-narusawa <swkei320@gmail.com>
2021-03-14 11:00:30 +0900

    implement event-show

--- src/index.js
+++ src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import EventsIndex from "./components/events_index";
 import EventsNew from "./components/events_new";
+import EventsShow from "./components/events_show";
 import "./index.css";
 import reducer from "./reducers";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const enhancer =
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk);
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
         <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/events/:id" component={EventsShow} />
         <Route exact path="" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
