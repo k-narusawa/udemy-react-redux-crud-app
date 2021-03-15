0c5a4f1094c2bd2f07abd20ff016e41eadda3db4
k-narusawa <swkei320@gmail.com>
2021-03-15 14:18:59 +0900

    implement material-ui events-index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -18,15 +19,17 @@ const enhancer =
 const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
-        <Route exact path="/events/:id" component={EventsShow} />
-        <Route exact path="" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/events/new" component={EventsNew} />
+          <Route exact path="/events/:id" component={EventsShow} />
+          <Route exact path="" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
